58475d0a85dfa11d2df104091d18112c9bc2e47c
vivid-acorn <84036300+vivid-acorn@users.noreply.github.com>
2022-02-26 10:55:38 +0000

    Add files via upload

---
+++ coordinater.py
@@ -0,0 +1,75 @@
+#how to find certain positions on a window in pygame
+#By vivid acorn
+#language = python
+
+import pygame
+
+
+pygame.font.init() 
+myFont = pygame.font.SysFont(None, 24)
+
+coordinate_history = []
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+file1 = open("myfile.txt","w")
+try:
+    file1.writelines(mx)
+except:
+    print('Data transfer failed.')
+    mfdecode = False
+
+
+
+pygame.init()
+ 
+size = (1450,1002)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("Window")
+ 
+done = False
+clock = pygame.time.Clock()
+
+def txt():
+    dis = myFont.render(str(pos), 1, BLACK)
+    #img = font.render(str(mx,my), True, (0,0,0))
+    screen.blit(dis, (20, 20))
+ 
+while not done:
+
+    mx,my = pygame.mouse.get_pos()
+    mx = pygame.mouse.get_pos()
+    screen.fill(WHITE)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            print(mx, my)
+            pos = mx, my
+            txt()
+            mfdecode = True
+
+    pos = mx,my
+    dis = myFont.render(str(pos), 1, BLACK)
+    textencode = myFont.render('Click a position to save.', 1, BLACK)
+    #img = font.render(str(mx,my), True, (0,0,0))
+    screen.blit(dis, (20, 20))
+    screen.blit(textencode, (20, 50))
+
+    
+ 
+    
+    
+ 
+  
+    pygame.display.flip()
+ 
+   
+    clock.tick(60)
+
+pygame.quit()
+file1.close()
